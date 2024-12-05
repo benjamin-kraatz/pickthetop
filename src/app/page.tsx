463146b6d3a1604294935a83ee3dedf93100c434
Benjamin Kraatz <benjaminkraatz@yahoo.com>
2024-12-05 13:58:50 +0100

    chore: remove unused auth from landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,12 @@
-import { currentUser } from "@clerk/nextjs/server";
 import LandingHeader from "~/components/LandingHeader";
 import { ThemeToggle } from "~/components/ui/theme-toggle";
 
-import { getUsername } from "~/server/auth/helpers";
-import { api, HydrateClient } from "~/trpc/server";
+import { HydrateClient } from "~/trpc/server";
 
 export default async function Home() {
-  const hello = await api.post.hello({ text: "from tRPC" });
-  const user = await currentUser();
-
-  if (user?.id) {
-    void api.post.getLatest.prefetch();
-  }
-
-  const username = getUsername(user);
-
   return (
     <HydrateClient>
-      <div className="absolute z-50 right-5 top-5">
+      <div className="absolute right-5 top-5 z-50">
         <ThemeToggle />
       </div>
       <main className="flex min-h-screen flex-col">

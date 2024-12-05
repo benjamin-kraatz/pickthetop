ce6dce343cc02a3bd810eedc24cff88779091a40
Benjamin Kraatz <benjaminkraatz@yahoo.com>
2024-12-05 12:56:07 +0100

    chore: add shadcn, including some components

--- src/app/login/[[...login]]/page.tsx
+++ src/app/login/[[...login]]/page.tsx
@@ -2,7 +2,7 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div className="mx-auto min-h-screen w-full flex flex-col items-center justify-center">
+    <div className="mx-auto flex min-h-screen w-full flex-col items-center justify-center">
       <SignIn />
     </div>
   );

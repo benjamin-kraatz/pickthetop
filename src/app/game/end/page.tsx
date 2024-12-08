75f276c4b406613413b35aa1773ce833c2f286e3
Benjamin Kraatz <benjaminkraatz@yahoo.com>
2024-12-08 14:54:16 +0100

    feat: add game/end page with CTAs for home and game

---
+++ src/app/game/end/page.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+import React from "react";
+import { buttonVariants } from "~/components/ui/button";
+
+export default function GameEndPage() {
+  return (
+    <div className="flex min-h-screen w-screen items-center justify-center">
+      <div className="text-center text-3xl font-medium">
+        Das Spiel ist vorbei!
+      </div>
+      <div className="h-4" />
+      <div className="text-center text-muted-foreground">
+        Vielen Dank für&apos;s Mitspielen!
+      </div>
+      <div className="h-4" />
+      <div className="flex items-center gap-2">
+        <Link href="/" className={buttonVariants({ variant: "outline" })}>
+          Zur Startseite
+        </Link>
+        <Link href="/game" className={buttonVariants({ variant: "default" })}>
+          Nochmal spielen
+        </Link>
+      </div>
+    </div>
+  );
+}
